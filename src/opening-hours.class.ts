4f109a2233e25f17e57ccffe13250023cd8dd9c1
Maes Constantin <constantinmaes@gmail.com>
2022-06-20 15:44:24 +0200

    :sparkles: feat(timezones): set timezone in constructor

--- src/opening-hours.class.ts
+++ src/opening-hours.class.ts
@@ -1,1 +1,18 @@
-export class OpeningHours {}
+import { DateTime, Settings } from 'luxon';
+
+export class OpeningHours {
+    readonly #timezone: string;
+
+    constructor(timezone: string = 'utc') {
+        this.#timezone = timezone;
+        if (this.#timezone) {
+            const init = DateTime.local().setZone(timezone);
+            if (!init.isValid && init.invalidReason === 'unsupported zone') {
+                throw new Error(
+                    'Invalid timezone passed to OpeningHours class'
+                );
+            }
+        }
+        Settings.defaultZone = 'utc';
+    }
+}
