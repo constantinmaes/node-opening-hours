f476be44095f532a5b9af4d09db132b97e643eff
Maes Constantin <constantinmaes@gmail.com>
2022-06-27 14:13:47 +0200

    :fire: rm: remove test code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
 import { OpeningHours } from './opening-hours.class';
 
-const test = new OpeningHours();
-
-test.create({
-    monday: ['09:00-17:00', '08:00-12:00'],
-    tuesday: ['06:00-11:00'],
-});
-const structured = test.toStructuredData();
-console.log(structured);
-
 export { OpeningHours };
